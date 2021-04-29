694a24efbd2f8a8ef2c5ff8357967929b3706e92
Joshua Findley <>
2021-04-29 02:27:42 +0000

    Added events for users to click on and learn more about the projects theme, music.

---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  document.getElementById("question1").innerHTML = "It depends on what you the music you like to listen to. For example, saxaphones for jazz music, acoustic guitar for country, or electric guitar for rock music.";
+}
+
+function myFunction2() {
+  document.getElementById("question2").innerHTML = "YOU CLICKED ME!";
+}
+
+function myFunction3() {
+  document.getElementById("question3").innerHTML = "YOU CLICKED ME!";
+}
+
+function myFunction4() {
+  document.getElementById("question4").innerHTML = "YOU CLICKED ME!";
+}
