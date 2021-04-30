5f6f8b5586a1cfd119d7c4aed393fa83b5deea56
Joshua Findley <>
2021-04-30 01:08:12 +0000

    Finish and editing before submitting.

--- script.js
+++ script.js
@@ -3,13 +3,31 @@ function myFunction() {
 }
 
 function myFunction2() {
-  document.getElementById("question2").innerHTML = "YOU CLICKED ME!";
+  document.getElementById("question2").innerHTML = "Please call the number at the bottom of the page for information on avaliable courses.";
 }
 
 function myFunction3() {
-  document.getElementById("question3").innerHTML = "YOU CLICKED ME!";
+  document.getElementById("question3").innerHTML = "You really never stop learning and practice makes perfect.";
 }
 
 function myFunction4() {
-  document.getElementById("question4").innerHTML = "YOU CLICKED ME!";
+  document.getElementById("question4").innerHTML = "Please go to the bottom of the page to email me directly.";
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
